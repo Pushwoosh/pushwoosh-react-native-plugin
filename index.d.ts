b650130078660dcd4a29a100a69b5423aee5b174
Vitaly Romanychev <vromanychev@pushwoosh.com>
2022-04-21 17:55:26 +0700

    [feature][PUSH-31457] Add typescript support

---
+++ index.d.ts
@@ -0,0 +1,57 @@
+declare module 'pushwoosh-react-native-plugin' {
+  interface PushwooshConfig {
+    pw_appid: string;
+    project_number: string;
+    pw_notification_handling?: string;
+  }
+  type LocalNotification = {
+    msg: string;
+    seconds: number;
+    userData?: Object;
+  }
+
+  interface Pushwoosh {
+    init(config: PushwooshConfig, success?: () => void, fail?: () => void);
+    createLocalNotification(notification: LocalNotification): void;
+    clearLocalNotification(): void;
+    clearNotificationCenter(): void;
+    register(success?: (token: string) => void, fail?: (error: Error) => void): void;
+    unregister(success?: (token: string) => void, fail?: (error: Error) => void): void;
+    onPushOpen(callback: () => void, fail?: ()=> void): void; 
+    setTags(
+      tags: Record<string, unknown>,
+      success?: () => void,
+      fail?: () => void
+    ): void;
+    getTags(success: () => void, fail?: () => void): void;
+    setShowPushnotificationAlert(showAlert: boolean): void;
+    getShowPushnotificationAlert(callback: (willShow: boolean) => void): void;
+    getPushToken(success?: (token: string) => void): void;
+    getHwid(success: (hwid: string) => void): void;
+    setUserId(userId: string, success?: ()=> void, fail?: (error: Error) => void): void;
+    postEvent(event: string, attributes?: Record<string, string>): void;
+    setApplicationIconBadgeNumber(badgeNumber: number): void;
+    getApplicationIconBadgeNumber(callback: (badge: number) => void): void;
+    addToApplicationIconBadgeNumber(badgeNumber: number): void;
+    setMultiNotificationMode(on: boolean): void;
+    setLightScreenOnNotification(on: boolean): void;
+    setEnableLED(on: boolean): void;
+    setColorLED(color: number): void;
+    setSoundType(type: number): void;
+    setVibrateType(type: number): void;
+    presentInboxUI(style?: Object): void;
+    showGDPRConsentUI(): void;
+    showGDPRDeletionUI(): void;
+    isDeviceDataRemoved(success: (isRemoved: boolean) => void): void;
+    isCommunicationEnabled(success: (isEnabled: boolean)=> void): void;
+    isAvailableGDPR(success: (isAvailable: boolean) => void): void;
+    setCommunicationEnabled(enabled: boolean, success?: () => void, fail?: (error: Error) => void): void;
+    removeAllDeviceData(success?: () => void, fail?: (error: Error) => void): void;
+    setNotificationIconBackgroundColor(color: string): void;
+    setLanguage(language: string): void;
+    enableHuaweiPushNotifications(): void;
+  }
+
+  declare const Pushwoosh: Pushwoosh;
+  export = Pushwoosh;
+}
