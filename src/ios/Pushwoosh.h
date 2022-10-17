d32171f7bd99c1a4a2269db2907cc06bada624f9
Jenkins CI <sdk@pushwoosh.com>
2022-10-17 15:34:08 +0700

    Pushwoosh React Native Plugin 6.1.16
    https://github.com/Pushwoosh/pushwoosh-react-native-plugin/releases/tag/6.1.16

--- src/ios/Pushwoosh.h
+++ src/ios/Pushwoosh.h
@@ -18,15 +18,15 @@
 
 #endif
 
-#define PUSHWOOSH_VERSION @"6.4.3"
+#define PUSHWOOSH_VERSION @"6.4.5"
 
 
 @class Pushwoosh, PWMessage, PWNotificationCenterDelegateProxy;
 
 
-typedef void (^PushwooshRegistrationHandler)(NSString *token, NSError *error);
-typedef void (^PushwooshGetTagsHandler)(NSDictionary *tags);
-typedef void (^PushwooshErrorHandler)(NSError *error);
+typedef void (^PushwooshRegistrationHandler)(NSString * _Nullable token, NSError * _Nullable error);
+typedef void (^PushwooshGetTagsHandler)(NSDictionary * _Nullable tags);
+typedef void (^PushwooshErrorHandler)(NSError * _Nullable error);
 
 
 /**
@@ -43,7 +43,7 @@ typedef void (^PushwooshErrorHandler)(NSError *error);
  @param pushwoosh The push manager that received the remote notification.
  @param message A PWMessage object that contains information referring to the remote notification, potentially including a badge number for the application icon, an alert sound, an alert message to display to the user, a notification identifier, and custom data.
 */
-- (void)pushwoosh:(Pushwoosh *)pushwoosh onMessageReceived:(PWMessage *)message;
+- (void)pushwoosh:(Pushwoosh * _Nonnull)pushwoosh onMessageReceived:(PWMessage * _Nonnull)message;
 
 /**
 Tells the delegate that the user has pressed on the push notification banner.
@@ -51,7 +51,32 @@ Tells the delegate that the user has pressed on the push notification banner.
 @param pushwoosh The push manager that received the remote notification.
 @param message A PWMessage object that contains information about the remote notification, potentially including a badge number for the application icon, an alert sound, an alert message to display to the user, a notification identifier, and custom data.
 */
-- (void)pushwoosh:(Pushwoosh *)pushwoosh onMessageOpened:(PWMessage *)message;
+- (void)pushwoosh:(Pushwoosh * _Nonnull)pushwoosh onMessageOpened:(PWMessage * _Nonnull)message;
+
+@end
+
+/**
+ `PWPurchaseDelegate` protocol defines the methods that can be implemented in the delegate of the `Pushwoosh` class' singleton object.
+ These methods provide callbacks for events related to purchasing In-App products from rich medias, such as successful purchase event, failed payment, etc.
+ These methods implementation allows to react on such events properly.
+ */
+
+@protocol PWPurchaseDelegate <NSObject>
+
+//list of products
+-(void)onPWInAppPurchaseHelperProducts:(NSArray<SKProduct *>* _Nullable)products;
+
+//payment complete
+-(void)onPWInAppPurchaseHelperPaymentComplete:(NSString* _Nullable)identifier;
+
+//payment failed
+-(void)onPWInAppPurchaseHelperPaymentFailedProductIdentifier:(NSString* _Nullable)identifier error:(NSError* _Nullable)error;
+
+//promoted In-App purchase
+-(void)onPWInAppPurchaseHelperCallPromotedPurchase:(NSString* _Nullable)identifier;
+
+//Tells the observer that an error occurred while restoring transactions.
+-(void)onPWInAppPurchaseHelperRestoreCompletedTransactionsFailed:(NSError * _Nullable)error;
 
 @end
 
@@ -64,17 +89,17 @@ Tells the delegate that the user has pressed on the push notification banner.
 /**
  Title of the push message.
 */
-@property (nonatomic, readonly) NSString *title;
+@property (nonatomic, readonly) NSString * _Nullable title;
 
 /**
  Subtitle of the push message.
 */
-@property (nonatomic, readonly) NSString *subTitle;
+@property (nonatomic, readonly) NSString * _Nullable subTitle;
 
 /**
  Body of the push message.
 */
-@property (nonatomic, readonly) NSString *message;
+@property (nonatomic, readonly) NSString * _Nullable message;
 
 /**
  Badge number of the push message.
@@ -89,7 +114,7 @@ Tells the delegate that the user has pressed on the push notification banner.
 /**
  Remote URL or deeplink from the push message.
 */
-@property (nonatomic, readonly) NSString *link;
+@property (nonatomic, readonly) NSString * _Nullable link;
 
 /**
  Returns YES if this message received/opened then the app is in foreground state.
@@ -109,17 +134,17 @@ Tells the delegate that the user has pressed on the push notification banner.
 /**
  Gets custom JSON data from push notifications dictionary as specified in Pushwoosh Control Panel.
 */
-@property (nonatomic, readonly) NSDictionary *customData;
+@property (nonatomic, readonly) NSDictionary * _Nullable customData;
 
 /**
  Original payload of the message.
 */
-@property (nonatomic, readonly) NSDictionary *payload;
+@property (nonatomic, readonly) NSDictionary * _Nullable payload;
 
 /**
  Returns YES if this message is recieved from Pushwoosh.
 */
-+ (BOOL)isPushwooshMessage:(NSDictionary *)userInfo;
++ (BOOL)isPushwooshMessage:(NSDictionary *_Nonnull)userInfo;
 
 @end
 
@@ -132,13 +157,18 @@ Tells the delegate that the user has pressed on the push notification banner.
 /**
  Pushwoosh Application ID. Usually retrieved automatically from Info.plist parameter `Pushwoosh_APPID`
  */
-@property (nonatomic, copy, readonly) NSString *applicationCode;
+@property (nonatomic, copy, readonly) NSString * _Nonnull applicationCode;
 
 /**
  `PushNotificationDelegate` protocol delegate that would receive the information about events for push notification manager such as registering with APS services, receiving push notifications or working with the received notification.
  Pushwoosh Runtime sets it to ApplicationDelegate by default
  */
-@property (nonatomic, weak) NSObject<PWMessagingDelegate> *delegate;
+@property (nonatomic, weak) NSObject<PWMessagingDelegate> * _Nullable delegate;
+
+/**
+ `PushPurchaseDelegate` protocol delegate that would receive the information about events related to purchasing InApp products from rich medias
+ */
+@property (nonatomic, weak) NSObject<PWPurchaseDelegate> * _Nullable purchaseDelegate;
 
 #if TARGET_OS_IOS || TARGET_OS_WATCH
 
@@ -157,62 +187,62 @@ Tells the delegate that the user has pressed on the push notification banner.
 /**
  Returns push notification payload if the app was started in response to push notification or null otherwise
  */
-@property (nonatomic, copy, readonly) NSDictionary *launchNotification;
+@property (nonatomic, copy, readonly) NSDictionary * _Nullable launchNotification;
 
 /**
  Proxy contains UNUserNotificationCenterDelegate objects.
 */
-@property (nonatomic, readonly) PWNotificationCenterDelegateProxy *notificationCenterDelegateProxy;
+@property (nonatomic, readonly) PWNotificationCenterDelegateProxy * _Nullable notificationCenterDelegateProxy;
 
 /**
  Set custom application language. Must be a lowercase two-letter code according to ISO-639-1 standard ("en", "de", "fr", etc.).
  Device language used by default.
  Set to nil if you want to use device language again.
  */
-@property (nonatomic) NSString *language;
+@property (nonatomic) NSString * _Nonnull language;
 
 /**
  Initializes Pushwoosh.
  @param appCode Pushwoosh App ID.
  */
-+ (void)initializeWithAppCode:(NSString *)appCode;
++ (void)initializeWithAppCode:(NSString *_Nonnull)appCode;
 
 /**
  Returns an object representing the current push manager.
  
  @return A singleton object that represents the push manager.
  */
-+ (instancetype)sharedInstance;
++ (instancetype _Nonnull )sharedInstance;
 
 /**
  Registers for push notifications. By default registeres for "UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert" flags.
  Automatically detects if you have "newsstand-content" in "UIBackgroundModes" and adds "UIRemoteNotificationTypeNewsstandContentAvailability" flag.
  */
 - (void)registerForPushNotifications;
-- (void)registerForPushNotificationsWithCompletion:(PushwooshRegistrationHandler)completion;
+- (void)registerForPushNotificationsWithCompletion:(PushwooshRegistrationHandler _Nullable )completion;
 
 /**
 Unregisters from push notifications.
 */
 - (void)unregisterForPushNotifications;
-- (void)unregisterForPushNotificationsWithCompletion:(void (^)(NSError *error))completion;
+- (void)unregisterForPushNotificationsWithCompletion:(void (^_Nullable)(NSError * _Nullable error))completion;
 
 /**
  Handle registration to remote notifications.
 */
-- (void)handlePushRegistration:(NSData *)devToken;
-- (void)handlePushRegistrationFailure:(NSError *)error;
+- (void)handlePushRegistration:(NSData * _Nonnull)devToken;
+- (void)handlePushRegistrationFailure:(NSError * _Nonnull)error;
 
 /**
  Handle received push notification.
 */
-- (BOOL)handlePushReceived:(NSDictionary *)userInfo;
+- (BOOL)handlePushReceived:(NSDictionary * _Nonnull)userInfo;
 
 /**
  * Change default base url to reverse proxy url
  * @param url - reverse proxy url
 */
-- (void)setReverseProxy:(NSString *)url;
+- (void)setReverseProxy:(NSString * _Nonnull)url;
 
 /**
  * Disables reverse proxy
@@ -235,16 +265,16 @@ Unregisters from push notifications.
  
  @param tags Dictionary representation of tags to send.
  */
-- (void)setTags:(NSDictionary *)tags;
+- (void)setTags:(NSDictionary * _Nonnull)tags;
 
 /**
  Send tags to server with completion block. If setTags succeeds competion is called with nil argument. If setTags fails completion is called with error.
  */
-- (void)setTags:(NSDictionary *)tags completion:(void (^)(NSError *error))completion;
+- (void)setTags:(NSDictionary * _Nonnull)tags completion:(void (^_Nullable)(NSError * _Nullable error))completion;
 
-- (void)setEmailTags:(NSDictionary *)tags forEmail:(NSString *)email;
+- (void)setEmailTags:(NSDictionary * _Nonnull)tags forEmail:(NSString * _Nonnull)email;
 
-- (void)setEmailTags:(NSDictionary *)tags forEmail:(NSString *)email completion:(void(^)(NSError *error))completion;
+- (void)setEmailTags:(NSDictionary * _Nonnull)tags forEmail:(NSString * _Nonnull)email completion:(void(^ _Nullable)(NSError * _Nullable error))completion;
 
 /**
  Get tags from server. Calls delegate method if exists and handler (block).
@@ -257,7 +287,7 @@ Unregisters from push notifications.
  }
  @param errorHandler The block is executed on the unsuccessful completion of the request. This block has no return value and takes one argument: the error that occurred during the request.
  */
-- (void)getTags:(PushwooshGetTagsHandler)successHandler onFailure:(PushwooshErrorHandler)errorHandler;
+- (void)getTags:(PushwooshGetTagsHandler _Nullable)successHandler onFailure:(PushwooshErrorHandler _Nullable)errorHandler;
 
 /**
  Sends current badge value to server. Called internally by SDK Runtime when `UIApplication` `setApplicationBadgeNumber:` is set. This function is used for "auto-incremeting" badges to work.
@@ -270,7 +300,7 @@ Unregisters from push notifications.
 /**
  Pushwoosh SDK version.
 */
-+ (NSString *)version;
++ (NSString * _Nonnull)version;
 
 #if TARGET_OS_IOS
 /**
@@ -285,7 +315,7 @@ Unregisters from push notifications.
  
  @param transactions Array of SKPaymentTransaction items as received in the payment queue.
  */
-- (void)sendSKPaymentTransactions:(NSArray *)transactions;
+- (void)sendSKPaymentTransactions:(NSArray * _Nonnull)transactions;
 
 /**
  Tracks individual in-app purchase. See recommended `sendSKPaymentTransactions:` method.
@@ -295,7 +325,7 @@ Unregisters from push notifications.
  @param currencyCode currency of the price (ex: @"USD")
  @param date time of the purchase (ex: [NSDate now])
  */
-- (void)sendPurchase:(NSString *)productIdentifier withPrice:(NSDecimalNumber *)price currencyCode:(NSString *)currencyCode andDate:(NSDate *)date;
+- (void)sendPurchase:(NSString * _Nonnull)productIdentifier withPrice:(NSDecimalNumber * _Nonnull)price currencyCode:(NSString * _Nonnull)currencyCode andDate:(NSDate * _Nonnull)date;
 
 #endif
 /**
@@ -303,7 +333,7 @@ Unregisters from push notifications.
  
  @return Current push token. May be nil if no push token is available yet.
  */
-- (NSString *)getPushToken;
+- (NSString * _Nullable)getPushToken;
 
 /**
  Gets HWID. Unique device identifier that used in all API calls with Pushwoosh.
@@ -311,7 +341,7 @@ Unregisters from push notifications.
  
  @return Unique device identifier.
  */
-- (NSString *)getHWID;
+- (NSString * _Nonnull)getHWID;
 
 /**
  Returns dictionary with enabled remove notificaton types.
@@ -341,7 +371,7 @@ Unregisters from push notifications.
  
  Note: In the latter example "enabled" field means that device can receive push notification but could not display alerts (ex: silent push)
  */
-+ (NSMutableDictionary *)getRemoteNotificationStatus;
++ (NSMutableDictionary * _Nullable)getRemoteNotificationStatus;
 
 /**
  Clears the notifications from the notification center.
@@ -355,7 +385,7 @@ Unregisters from push notifications.
  
  @param userId user identifier
  */
-- (void)setUserId:(NSString *)userId completion:(void(^)(NSError * error))completion;
+- (void)setUserId:(NSString * _Nonnull)userId completion:(void(^ _Nullable)(NSError * _Nullable error))completion;
 
 /**
  Set User indentifier. This could be Facebook ID, username or email, or any other user ID.
@@ -363,7 +393,7 @@ Unregisters from push notifications.
  
  @param userId user identifier
  */
-- (void)setUserId:(NSString *)userId;
+- (void)setUserId:(NSString * _Nonnull)userId;
 
 /**
  Set User indentifier. This could be Facebook ID, username or email, or any other user ID.
@@ -373,7 +403,7 @@ Unregisters from push notifications.
  @param userId user identifier
  @param emails user's emails array
  */
-- (void)setUser:(NSString *)userId emails:(NSArray *)emails completion:(void(^)(NSError * error))completion;
+- (void)setUser:(NSString * _Nonnull)userId emails:(NSArray * _Nonnull)emails completion:(void(^ _Nullable)(NSError * _Nullable error))completion;
 
 
 /**
@@ -383,7 +413,7 @@ Unregisters from push notifications.
  @param userId user identifier
  @param emails user's emails array
  */
-- (void)setUser:(NSString *)userId emails:(NSArray *)emails;
+- (void)setUser:(NSString * _Nonnull)userId emails:(NSArray * _Nonnull)emails;
 
 /**
  Set User indentifier. This could be Facebook ID, username or email, or any other user ID.
@@ -393,7 +423,7 @@ Unregisters from push notifications.
  @param userId user identifier
  @param email user's email string
  */
-- (void)setUser:(NSString *)userId email:(NSString *)email completion:(void(^)(NSError * error))completion;
+- (void)setUser:(NSString * _Nonnull)userId email:(NSString * _Nonnull)email completion:(void(^ _Nullable)(NSError * _Nullable error))completion;
 
 /**
  Register emails list associated to the current user.
@@ -401,14 +431,14 @@ Unregisters from push notifications.
  
  @param emails user's emails array
  */
-- (void)setEmails:(NSArray *)emails completion:(void(^)(NSError * error))completion;
+- (void)setEmails:(NSArray * _Nonnull)emails completion:(void(^ _Nullable)(NSError * _Nullable error))completion;
 
 /**
  Register emails list associated to the current user.
  
  @param emails user's emails array
  */
-- (void)setEmails:(NSArray *)emails;
+- (void)setEmails:(NSArray * _Nonnull)emails;
 
 /**
  Register email associated to the current user. Email should be a string and could not be null or empty.
@@ -416,14 +446,14 @@ Unregisters from push notifications.
  
  @param email user's email string
  */
-- (void)setEmail:(NSString *)email completion:(void(^)(NSError * error))completion;
+- (void)setEmail:(NSString * _Nonnull)email completion:(void(^ _Nullable)(NSError * _Nullable error))completion;
 
 /**
  Register email associated to the current user. Email should be a string and could not be null or empty.
  
  @param email user's email string
  */
-- (void)setEmail:(NSString *)email;
+- (void)setEmail:(NSString * _Nonnull)email;
 
 /**
  Move all events from oldUserId to newUserId if doMerge is true. If doMerge is false all events for oldUserId are removed.
@@ -433,7 +463,7 @@ Unregisters from push notifications.
  @param doMerge if false all events for oldUserId are removed, if true all events for oldUserId are moved to newUserId
  @param completion callback
  */
-- (void)mergeUserId:(NSString *)oldUserId to:(NSString *)newUserId doMerge:(BOOL)doMerge completion:(void (^)(NSError *error))completion;
+- (void)mergeUserId:(NSString * _Nonnull)oldUserId to:(NSString * _Nonnull)newUserId doMerge:(BOOL)doMerge completion:(void (^ _Nullable)(NSError * _Nullable error))completion;
 
 /**
  Starts communication with Pushwoosh server.
@@ -451,7 +481,7 @@ Unregisters from push notifications.
  @param url Deep Link URL
 */
 #if TARGET_OS_IOS || TARGET_OS_WATCH
-- (BOOL)handleOpenURL:(NSURL *)url;
+- (BOOL)handleOpenURL:(NSURL * _Nonnull)url;
 #endif
 
 @end
@@ -468,7 +498,7 @@ Unregisters from push notifications.
  Returns UNUserNotificationCenterDelegate that handles foreground push notifications on iOS10
 */
 #if TARGET_OS_IOS || TARGET_OS_WATCH
-@property (nonatomic, strong, readonly) id<UNUserNotificationCenterDelegate> defaultNotificationCenterDelegate;
+@property (nonatomic, strong, readonly) id<UNUserNotificationCenterDelegate> _Nonnull defaultNotificationCenterDelegate;
 #elif TARGET_OS_OSX
 @property (nonatomic, strong, readonly) id<NSUserNotificationCenterDelegate> defaultNotificationCenterDelegate;
 #endif
@@ -477,7 +507,7 @@ Unregisters from push notifications.
  Adds extra UNUserNotificationCenterDelegate that handles foreground push notifications on iOS10.
 */
 #if TARGET_OS_IOS || TARGET_OS_WATCH
-- (void)addNotificationCenterDelegate:(id<UNUserNotificationCenterDelegate>)delegate;
+- (void)addNotificationCenterDelegate:(id<UNUserNotificationCenterDelegate> _Nonnull)delegate;
 #endif
 @end
 
@@ -504,7 +534,7 @@ Unregisters from push notifications.
  
  @return Dictionary, that needs to be sent as the value for the tag
  */
-+ (NSDictionary *)incrementalTagWithInteger:(NSInteger)delta;
++ (NSDictionary * _Nullable)incrementalTagWithInteger:(NSInteger)delta;
 
 /**
  Creates a dictionary for extending Tag’s values list with additional values
@@ -525,6 +555,6 @@ Unregisters from push notifications.
  
  @return Dictionary to be sent as the value for the tag
  */
-+ (NSDictionary *)appendValuesToListTag:(NSArray<NSString *> *)array;
++ (NSDictionary * _Nullable)appendValuesToListTag:(NSArray<NSString *> * _Nonnull)array;
 
 @end
